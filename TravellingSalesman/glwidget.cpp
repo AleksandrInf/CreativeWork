5dbbf0c55c3cd03d5fb93c1c81a09356f4e7f5a8
AleksandrInf <117199080+AleksandrInf@users.noreply.github.com>
2023-06-11 15:38:31 +0300

    Update glwidget.cpp

--- TravellingSalesman/glwidget.cpp
+++ TravellingSalesman/glwidget.cpp
@@ -1,23 +1,18 @@
 #include "glwidget.h"
 #include "top.h"
 #include "edge.h"
-
 #include <QPainter>
-
 extern QVector<TOP> tops;
 extern QVector<EDGE> edges;
-
 GlWidget::GlWidget(QWidget *parent)
     :QOpenGLWidget(parent)
 {
     mypainter=new Painter;
 }
-
 void GlWidget::redraw()
 {
    this->update();
 }
-
 void GlWidget::paintEvent(QPaintEvent *event)
 {
     QPainter painter;
